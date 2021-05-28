a2efd0dbcc4b749d6a1bcb334bde83f06b3a4de5
leducquan24 <71084011+leducquan24@users.noreply.github.com>
2021-05-28 21:22:45 +0700

    LeDucQuan

---
+++ T2009M_AP/S1.cs
@@ -0,0 +1,22 @@
+﻿namespace T2009M_AP
+{
+    public class S1
+    {
+        public int id;
+        public string name;
+        public string price;
+        public string qty;
+
+        public lab1()
+        {
+        }
+
+        public lab1(int id, string name, string price, string qty)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.qty = qty;
+        }
+    }
+}
