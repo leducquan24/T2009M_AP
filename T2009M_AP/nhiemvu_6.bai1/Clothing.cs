f7729b429edf50945af40a2a31184aedb58a2142
leducquan24 <71084011+leducquan24@users.noreply.github.com>
2021-05-31 17:10:22 +0700

    nop nhiemvu6.bai1

---
+++ T2009M_AP/nhiemvu_6.bai1/Clothing.cs
@@ -0,0 +1,9 @@
+﻿namespace T2009M_AP.nhiemvu_6.bai1
+{
+    public class Clothing
+    {
+        public abstract void Sales();
+        public abstract void Materials();
+        public abstract void CommonWear();
+    }
+}
